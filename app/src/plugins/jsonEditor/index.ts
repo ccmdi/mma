67ff53b32528c358f105cf91ee5a39023733262b
ccmdi <ccmdi.dev@gmail.com>
2026-07-21 21:55:18 -0400

    chore: register the JSON editor plugin in dev builds only

--- app/src/plugins/jsonEditor/index.ts
+++ app/src/plugins/jsonEditor/index.ts
@@ -2,11 +2,13 @@ const { registerPlugin } = window.MMA;
 import { JsonEditorPanel } from "./JsonEditorPanel";
 import { mdiCodeBraces } from "@mdi/js";
 
-registerPlugin({
-	id: "json-editor",
-	name: "JSON editor",
-	description: "View and edit location data as JSON",
-	icon: mdiCodeBraces,
-	activate() {},
-	locationPanel: JsonEditorPanel,
-});
+if (import.meta.env.DEV) {
+	registerPlugin({
+		id: "json-editor",
+		name: "JSON editor",
+		description: "View and edit location data as JSON",
+		icon: mdiCodeBraces,
+		activate() {},
+		locationPanel: JsonEditorPanel,
+	});
+}
